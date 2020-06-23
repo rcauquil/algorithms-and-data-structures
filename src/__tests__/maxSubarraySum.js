47350b02037574c43670507b2bf08ee5125c64df
Romain Cauquil <romaincauquilrc@gmail.com>
2020-06-23 10:43:11 +0200

    init

---
+++ src/__tests__/maxSubarraySum.js
@@ -0,0 +1,27 @@
+const maxSubarraySum = require('../maxSubarraySum.js');
+
+describe('Compute the max sum of a given subarray length using: "Sliding Window" Tc:O(n) Sc:O(1)', () => {
+  test('[100,200,300,400], 2 : [300,400]=700', () => {
+    expect(maxSubarraySum([100,200,300,400], 2)).toBe(700);
+  });
+
+  test('[1,4,2,10,23,3,1,0,20], 4 : [4,10,23,3]=39', () => {
+    expect(maxSubarraySum([1,4,2,10,23,3,1,0,20], 4)).toBe(39);
+  });
+
+  test('[-3,4,0,-2,6,-1], 2 : [6,-1]=5', () => {
+    expect(maxSubarraySum([-3,4,0,-2,6,-1], 2)).toBe(5);
+  });
+
+  test('[3,-2,7,-4,1,-1,4,-2,1], 2 : [-2,7]=5', () => {
+    expect(maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1], 2)).toBe(5);
+  });
+
+  test('[2,3], 3 : null', () => {
+    expect(maxSubarraySum([2,3], 3)).toBe(null);
+  });
+
+  test('[], 2 : null', () => {
+    expect(maxSubarraySum([], 2)).toBe(null);
+  });
+});
---
+++ src/maxSubarraySum.js
@@ -0,0 +1,29 @@
+/**
+ * Compute the max sum of a given subarray length using:
+ * "Sliding Window" Tc:O(n) Sc:O(1)
+ * 
+ * @param {Array}     a the array to parse 
+ * @param {Number}    n length of the sub array
+ * @returns {Number}  the max sum
+ */
+function maxSubarraySum(a, n) {
+  // Condition
+  if (!a.length || a.length < n) return null;
+
+  // First window
+  let maxSum = 0;  
+  for (let i = 0; i < n; i++) {
+    maxSum += a[i];
+  }
+
+  // Move the window to the end
+  let tempSum = maxSum;
+  for (let i = n; i < a.length; i++) {
+    tempSum += a[i] - a[i-n];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+
+  return maxSum;
+}
+
+module.exports = maxSubarraySum;
