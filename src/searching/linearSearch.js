54dafce71cc9e8b6ea6ea8132b5313501224368e
Romain Cauquil <romaincauquilrc@gmail.com>
2020-07-05 15:36:50 +0200

    add comments

--- src/searching/linearSearch.js
+++ src/searching/linearSearch.js
@@ -1,3 +1,11 @@
+/**
+ * Linear Search
+ * Time:O(n) Space:O(1)
+ * 
+ * @param {array} a 
+ * @param {number} b 
+ * @returns {number}
+ */
 function linearSearch(a, b) {
   if (!a.length) return -1;
   for (let i=0; i<a.length; i++) {
